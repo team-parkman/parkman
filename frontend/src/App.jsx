71490ff2854d799aebb7522e703413f0849d6366
Victor Yohanna <victoryohanna@gmail.com>
2023-11-18 07:09:42 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+//import './App.css'
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <h1 className="text-3xl font-bold  text-center">Parkman</h1>
+    </div>
+  );
+}
+
+export default App;
