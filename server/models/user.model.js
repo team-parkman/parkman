feb97b6a38743ef8a309f71eed1fc67f1b19b73d
Victor Yohanna <you@example.com>
2024-01-10 17:29:54 +0100

    renamed user model

---
+++ server/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const UserSchema = schema({
+    username:{
+        type: String,
+        require: true
+    },
+    email:{
+        type: String,
+        require: true
+    },
+    password:{
+        type: String,
+        require: true
+    },
+    confirmPassword:{
+        type: String
+    },
+    userType:{
+        type: String,
+        require: true
+    }
+})
+
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
